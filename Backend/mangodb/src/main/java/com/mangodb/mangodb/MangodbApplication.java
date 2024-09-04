8d61acc971bd6853553cf9234bac75ad468bfd3b
Ali Osman Ali Taha <65508729+aliotaha@users.noreply.github.com>
2024-09-04 19:53:56 +0100

    Update MangodbApplication.java
    comments

--- Backend/mangodb/src/main/java/com/mangodb/mangodb/MangodbApplication.java
+++ Backend/mangodb/src/main/java/com/mangodb/mangodb/MangodbApplication.java
@@ -3,12 +3,18 @@ package com.mangodb.mangodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * The entry point of the Spring Boot application.
+ * This class is responsible for starting the application.
+ */
 @SpringBootApplication
 public class MangodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MangodbApplication.class, args);
-	}
-
+    /**
+     * Main method to run the Spring Boot application.
+     * @param args Command-line arguments.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MangodbApplication.class, args);
+    }
 }
