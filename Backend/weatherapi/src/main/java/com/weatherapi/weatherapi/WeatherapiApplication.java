abd3ca8aa7ff097d51a8f548729d58ed4de4992c
Ali Osman Ali Taha <65508729+aliotaha@users.noreply.github.com>
2024-09-04 19:01:49 +0100

    Update WeatherapiApplication.java
    comments

--- Backend/weatherapi/src/main/java/com/weatherapi/weatherapi/WeatherapiApplication.java
+++ Backend/weatherapi/src/main/java/com/weatherapi/weatherapi/WeatherapiApplication.java
@@ -5,13 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-@SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.weatherapi.weatherapi.repository")
-@EnableScheduling
+/**
+ * The main entry point for the Weather API Spring Boot application.
+ * This class is responsible for bootstrapping the application,
+ * configuring the necessary Spring Boot components, and enabling specific features like MongoDB repositories and scheduling.
+ */
+@SpringBootApplication // Marks this class as a Spring Boot application and triggers auto-configuration, component scanning, etc.
+@EnableMongoRepositories(basePackages = "com.weatherapi.weatherapi.repository") // Enables the creation of MongoDB repositories in the specified package.
+@EnableScheduling // Enables support for scheduled tasks within the application.
 public class WeatherapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherapiApplication.class, args);
-	}
+    /**
+     * The main method that serves as the entry point for the Spring Boot application.
+     * This method delegates to Spring Boot's SpringApplication.run method to launch the application.
+     *
+     * @param args Command-line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherapiApplication.class, args); // Starts the Spring Boot application.
+    }
 }
