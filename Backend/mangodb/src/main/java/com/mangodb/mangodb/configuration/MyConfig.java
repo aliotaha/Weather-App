42e59d5f2bedffc64b93fb8f8fa12c369ecf94ef
Ali Osman Ali Taha <65508729+aliotaha@users.noreply.github.com>
2024-09-04 19:55:04 +0100

    Update MyConfig.java
    comments

--- Backend/mangodb/src/main/java/com/mangodb/mangodb/configuration/MyConfig.java
+++ Backend/mangodb/src/main/java/com/mangodb/mangodb/configuration/MyConfig.java
@@ -4,22 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+/**
+ * Configuration class for security-related beans.
+ */
 @Configuration
 public class MyConfig {
 
-
+    /**
+     * Bean for encoding passwords using BCrypt.
+     * BCrypt is a strong hashing algorithm that includes a salt to protect against rainbow table attacks.
+     * @return PasswordEncoder instance.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Bean for managing authentication.
+     * This manager is used for authentication operations in Spring Security.
+     * @param configuration AuthenticationConfiguration object.
+     * @return AuthenticationManager instance.
+     * @throws Exception if there is an issue retrieving the authentication manager.
+     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
